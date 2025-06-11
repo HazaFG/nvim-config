681fde6ce2cd91de34ac6cdc455be06bc14428cb
HazaFG <hazaelfg@gmail.com>
2025-06-11 11:09:30 -0700

    nvimtree a la derecha

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-tree/nvim-tree.lua",
   opts = {
+    view = {
+      side = "right"
+    },
     filters = {
       dotfiles = false,       -- Mostrar archivos ocultos (.gitignore, .env, etc.)
       git_ignored = false,    -- Mostrar archivos ignorados por Git
